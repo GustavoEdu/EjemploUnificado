62f3b0585ebcc67e95963a69cdc37746ab4a9476
Gustavo <gordono@unsa.edu.pe>
2021-11-05 15:09:36 +0000

    se corrigio como el contador estaba inicializado, ademas de que subsano el error sobre que el programa no imprimia la ultima columna de la informacion casi estructurada

--- lectura.pl
+++ lectura.pl
@@ -4,10 +4,11 @@ use warnings;
 
 open(IN, "Programas de Universidades.csv") or die("Error al abrir el archivo");
 my $line = <IN>;
-my $counter = 0;
+my $counter = 1;
 while($line =~ /^([^\|]+)\|(.+)/){
   print "[$counter] $1\n";
   $counter++;
   $line = $2;
 }
+print "[$counter] $line\n";
 close(IN);
