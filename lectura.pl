39f62b9e51c7279e6a84770e63abd62ca591fe2c
Gustavo <gordono@unsa.edu.pe>
2021-11-05 16:29:30 +0000

    se encontro una posible solucion a la consigna

--- lectura.pl
+++ lectura.pl
@@ -3,7 +3,12 @@ use strict;
 use warnings;
 
 open(IN, "Programas de Universidades.csv") or die("Error al abrir el archivo");
-my $line = <IN>;
+
+my @arr = <IN>;
+
+close(IN);
+
+my($line) = @arr;
 my $counter = 1;
 while($line =~ /^([^\|]+)\|(.+)/){
   print "[$counter] $1\n";
@@ -11,4 +16,3 @@ while($line =~ /^([^\|]+)\|(.+)/){
   $line = $2;
 }
 print "[$counter] $line\n";
-close(IN);
