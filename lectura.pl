03258f9da3cb67020fb42ff211e397bf726cbc50
Gustavo <gordono@unsa.edu.pe>
2021-11-05 15:07:35 +0000

    se agrego un contador que recorra todo el ciclo while y nos indique la cantidad de columnas que imprime el programa

--- lectura.pl
+++ lectura.pl
@@ -4,8 +4,10 @@ use warnings;
 
 open(IN, "Programas de Universidades.csv") or die("Error al abrir el archivo");
 my $line = <IN>;
+my $counter = 0;
 while($line =~ /^([^\|]+)\|(.+)/){
-  print "$1\n";
+  print "[$counter] $1\n";
+  $counter++;
   $line = $2;
 }
 close(IN);
