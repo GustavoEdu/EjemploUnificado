169551cbe982007efc57d5efe4fd5617cd13767d
Gustavo <gordono@unsa.edu.pe>
2021-11-12 04:37:54 +0000

    Se suprimio las lineas que causaban ruido al codigo, previamente salvadas en el archivo de web y error, luego se renombraron las subrutinas realizadas a nombres mas descriptivos (sujeto a mejora), ademas que se modifico la subrutina realizada para que ya no devuelve un arreglo unidimensional de Strings, sino un arreglo asociativo, puesto a que asi lo recalcaba la consigna, se eliminaaron los comentarios que causaban ruido a la zona operativa. Finalmente, se puso en aplicacion las subrutinas y se probo exitosamente el programa, el cual ya esta listo para ejectarse (permisos añadidos 755 y 755 a todos los documentos por si acaso)

--- cgi-bin/lectura.pl
+++ cgi-bin/lectura.pl
@@ -4,9 +4,9 @@ use warnings;
 use CGI;
 
 my $q = CGI->new;
-my $lugar = $q->param("lugar");
+my $region = $q->param("lugar");
 print $q->header("text/html");
-print<<BLOCK;
+print<<HTML;
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -15,104 +15,75 @@ print<<BLOCK;
 </head>
 <body>
   <h1>Resultados de la Consulta:</h1>
-BLOCK
+HTML
 
 #Leyendo todo el archivo en un arreglo de Strings
 open(IN, "Programas de Universidades.csv") or die("Error al abrir el archivo");
 my @arr = <IN>;
 close(IN);
 
-my $size = leerEncabezado();
-my $pattern = construirRegExp($size);
-print STDERR "$pattern\n"; #Se verifica si el patron generado es correcto
+#Aplicacion de las Subrutinas
+my %universidades = recolectarUniversidades($region, "Programas de Universidades.csv");
+mostrarArreglo(%universidades);
 
-#my %unis = ();
-#foreach my $linea (@arr) {
-#if($linea =~ /$pattern/ && $11 eq "AREQUIPA") {
-#print "$2\n"; Hay repeticion de los nombres
-#$unis{$2} = "";
-#}
-#}
-
-#Se recorre todo el Archivo, linea por linea
-#foreach my $linea (@arr) {
-#  if($linea =~ /$pattern/) {
-#    my $departamento = $11; 
-#    my $universidad = $2;
-#    if($departamento eq "AREQUIPA") {
-#      my $cont = $unis{$universidad};
-#      if(!defined($cont)) {
-#        $unis{$universidad} = 1;
-#      } else {
-#        $cont++;
-#        $unis{$universidad} = $cont;
-#      }
-#    }
-#  }
-#}
-
-#my $sum = 0;
-#foreach my $universidad (keys %unis) {
-#  my $num_carreras = $unis{$universidad};
-#  print "$universidad:$num_carreras\n"; #STDOUT
-  #$sum += $unis{$uni};
-  #}
-#print "$sum\n";
-
-  #my @array = mostrarUnisPorDepartamento("CUSCO", "Programas de Universidades.csv");
-
-  #print "Introduzca el nombre de una Region: ";
-  #my $region = <STDIN>; 
-  #chomp($region);
-  #print "Universidades que tiene sede en la Region $region:\n";
-my @array = mostrarUnisPorDepartamento($lugar, "Programas de Universidades.csv");
-foreach my $university (@array) {
-  print "<p>$university</p>\n";
-}
-
+#Se imprime la cola del texto HTML
 print<<HTML;
 </body>
 </html>
 HTML
 
-sub mostrarUnisPorDepartamento() {
+#Subrutina que devuelve un array de Strings de Universidades segun Region
+sub recolectarUniversidades {
   my $str = $_[0];
   my $fileName = $_[1];
 
   open(FILE, $fileName) or die("Error al abrir el Archivo!!!");
   my @arreglo = <FILE>; 
   close(FILE);
+  
+  my $size = contarColumnas($arreglo[0]);
+  my $pattern = construirRegExp($size);
 
   my %universidades = ();
-
-  foreach my $line (@arreglo) {
-    if($line =~ /$pattern/) {
+  foreach my $linea (@arreglo) {
+    if($linea =~ /$pattern/) {
       my $departamento = $11; 
       my $universidad = $2;
-      if($departamento eq $str) {
-        $universidades{$universidad} = "";
+      if($departamento eq "AREQUIPA") {
+        my $cont = $universidades{$universidad};
+        if(!defined($cont)) {
+          $universidades{$universidad} = 1;
+        } else {
+        $cont++;
+        $universidades{$universidad} = $cont;
+        } 
       }
     }
   }
-  my @lista;
-  foreach my $uni (keys %universidades) {
-    push(@lista, $uni);
-  }
-  return @lista;
+  return %universidades;
 }
 
-sub leerEncabezado {
+#Subrutina que muestra el resultado de la consulta, ademas de
+#transformarlo en texto HTML 
+sub mostrarArreglo {
+  my %dict = @_;
+  foreach my $universidad (keys %universidades) {
+    print "<p>$universidad: $universidades{$universidad}</p>\n";
+  }
+}
+
+#Subrutina que cuenta el Numero de Columnas del Encabezado
+sub contarColumnas {
   my $line = $arr[0];
   my $counter = 1;
   while($line =~ /^([^\|]+)\|(.+)/){
-    print  STDERR "[$counter] $1\n";
     $counter++;
     $line = $2;
   }
-  print STDERR "[$counter] $line\n";
-  #print "$counter\n";
   return $counter;
 }
+
+#Subrutina que construye la Expresion Regular segun Extension
 sub construirRegExp {
   my $num = $_[0]; 
   my $s = "^";
